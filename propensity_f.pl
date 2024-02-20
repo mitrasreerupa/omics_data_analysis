e271ead34cc07c5ef21321631fc4212a193dfb85
Sreerupa Mitra <123728237+mitrasreerupa@users.noreply.github.com>
2024-02-20 22:46:17 +0530

    Add files via upload
    To calculate the propensity of amino acids in an alpha helix of a protein structure.

---
+++ propensity_f.pl
@@ -0,0 +1,102 @@
+#to find the propensity of an amino acid in a database
+$directory = 'C:\Users\LENOVO\Desktop\M.Sc. Clg\Assignments\SB\Practical 4';
+opendir (DIR, $directory) or die "File not found,$!";
+while ($file = readdir DIR)
+{
+	if ($file=~/.pdb$/)
+	{
+		push @name,$file;		
+	}
+}
+foreach $n(@name)
+{
+	open (F, $n);
+	{
+		%per_db=();
+		while($line=<F>)
+		{
+		if($line=~/(^SEQRES).*([0-9]{3}) (.*)....\s/)
+		{	
+		chomp($j=$3);
+		@j=split/ /,$j;
+		push(@b,@j);
+		} 
+		}		
+	}
+	close(F);				
+}
+	
+print "@b\n";
+$l = @b;
+print"Length = $l\n;";
+$last=$#b;	
+
+%per_helix=();
+foreach $n(@name)
+{
+	open (F, $n);
+	{
+		
+		while ($line=<F>)
+		{
+			if($line=~/^HELIX   ..  .. (...) .  ...  (...) .  ...  .                                  (..).*/)
+			{
+
+				
+				for($i=0;$i<=$last;$i++)
+				{
+					
+					if ($b[$i] eq $1)
+					{
+						
+						if($b[$i+$3-1] eq $2)
+						{
+						 for($m=$i;$m<=$i+$3-1;$m++)
+						 {
+						 	push (@helix,$b[$m]);
+						 	$h++;
+						 	
+						 }
+						 last;
+						}
+					}
+				}	
+				}
+			 }
+			}
+			
+		
+	close(F);
+}
+foreach $aa(@b)
+{
+	$per_db{$aa}++;
+}
+foreach $k(keys%per_db)
+{
+	print "$k = $per_db{$k}\n";
+}
+print"@helix\n";
+$ht=@helix;
+print"$l1\n";
+foreach $hh(@helix)
+{
+	$per_helix{$hh}++;
+}
+foreach $q(keys%per_helix)
+{
+	print "$q = $per_helix{$q}\n";
+}
+chomp($input=<>);
+if (exists($per_helix{$input}))
+{
+	print "$per_helix{$input}\n";
+	$ha= $per_helix{$input};
+}
+if (exists($per_db{$input}))
+{
+	print"$per_db{$input}\n";
+	$seqt=$per_db{$input};
+}
+$propensity=($ha/$ht)/($seqt/$l);
+print "Propensity = $propensity\n";
